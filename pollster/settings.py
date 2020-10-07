60ccaa48df9a9f1bf608999e197f4a9c8db9bdfe
Mrityunjaya Hatagundi (mhatagun) <mhatagun@cisco.com>
2020-10-07 23:55:25 +0530

    commit r

--- pollster/settings.py
+++ pollster/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ayl0uawxbrh5-@unj4ky7@=mf8h_m_-y6c46-#(!f^w42(vx-w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['voting-lansw.herokuapp.com','127.0.0.1']
 
